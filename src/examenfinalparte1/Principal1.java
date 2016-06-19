a479fd3ecce8b902317a33098675cf8c8ac7dc03
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-19 22:34:02 +0200

    nuevo nombre a las variables para que quede mas claro lo que hace cada una

---
+++ src/examenfinalparte1/Principal1.java
@@ -0,0 +1,15 @@
+package examenfinalparte1;
+import java.io.IOException;
+
+public class Principal1 {
+    
+public static void main(String[] args) throws IOException{
+Principal2 obj = new Principal2(10,20);
+int resultOp1 = obj.resultado;
+System.out.println("Es: " + resultOp1);
+Principal2 nm = new Principal2(10);
+int resultOp2 = nm.resultado;
+System.out.println("Es: " + resultOp2);
+Principal2 mn = new Principal2();
+int resultOp3 = mn.resultado;
+System.out.println("Es: " + resultOp3);}}
