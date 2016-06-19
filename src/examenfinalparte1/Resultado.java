c9261c131019320248089266b3829fc4294708f7
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-19 23:17:00 +0200

    Javadoc clase main

--- src/examenfinalparte1/Resultado.java
+++ src/examenfinalparte1/Resultado.java
@@ -4,12 +4,24 @@ import java.io.IOException;
 public class Resultado {
     
 public static void main(String[] args) throws IOException{
-Principal2 obj = new Principal2(10,20);
+ /**
+ * Creamos un objeto de tipo Operacion, que recibe dos paámetros
+ * Con esas dos variables hará la suma y se visualizara.
+ */
+Operacion obj = new Operacion(10,20);
 int resultOp1 = obj.resultado;
 System.out.println("Es: " + resultOp1);
-Principal2 nm = new Principal2(10);
+/** 
+ * Tenemos otro objeto de tipo Operacion, pero esta vez recibe un solo parámetro
+ * El valor de esa variabl se mostrará por pantalla
+ */
+Operacion nm = new Operacion(10);
 int resultOp2 = nm.resultado;
 System.out.println("Es: " + resultOp2);
-Principal2 mn = new Principal2();
+/**
+* El metodo resultado aqui tiene un valor de -1, ese valor se le asigna a la variable resultOp3.
+* Y se nos visulazaria el valor de esa variable.
+*/
+Operacion mn = new Operacion();
 int resultOp3 = mn.resultado;
 System.out.println("Es: " + resultOp3);}}
