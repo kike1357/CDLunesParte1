f8e22cd0c7015c1f78cb8ec82aaef4926be43fa2
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-19 22:28:16 +0200

    cambiado el nombre de la variable i

--- src/examenfinalparte1/Principal2.java
+++ src/examenfinalparte1/Principal2.java
@@ -1,9 +1,14 @@
 package examenfinalparte1;
+
 public class Principal2 {
-    int i;
-public Principal2(int j, int m) {i=met(j,m);}
+    int resultado;
+    
+public Principal2(int j, int m) {resultado=met(j,m);}
+
     public Principal2(int j) {
-        i=met(j,j);}
-    public Principal2() { i=-1;
+        resultado=met(j,j);}
+    
+    public Principal2() { resultado=-1;
+    
     }public int met(int x, int x1) {
       return x + x1;}}
