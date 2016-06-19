f0b637ae38da89191dbb70d3fa116b61c14ce644
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-19 22:22:00 +0200

    Renombrado el nombre de las clases

---
+++ src/examenfinalparte1/Principal2.java
@@ -0,0 +1,9 @@
+package examenfinalparte1;
+public class Principal2 {
+    int i;
+public Principal2(int j, int m) {i=met(j,m);}
+    public Principal2(int j) {
+        i=met(j,j);}
+    public Principal2() { i=-1;
+    }public int met(int x, int x1) {
+      return x + x1;}}
